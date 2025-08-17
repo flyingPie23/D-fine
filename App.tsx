432bd0a8883df79a28494cb5d8acf865f20acbff
driss <freah>
2025-08-17 23:28:32 +0100

    set up home page

--- App.tsx
+++ App.tsx
@@ -1,8 +1,47 @@
+import React, { useState } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import InputField from './components/InputField';
 
 export default function App() {
+
+  const [query, setQuery] = useState<string>("");
+  const [definitions, SetDefinitions] = useState<string>("");
+
+  const define = async (query: string) => {
+    SetDefinitions("");
+    try {
+      const response = await fetch(`https://api.api-ninjas.com/v1/dictionary?word=${query}`, {
+        method: 'GET',
+        headers: {
+          'X-Api-Key': 'PAlItrTsiI0ZtJYUXslE/w==zYhuYLq3rBW5nLCY'
+        }
+      });
+      const data = await response.json();
+      if (data.valid === true) {
+        SetDefinitions(data.definition);
+      } else {
+        SetDefinitions("No available definitions for the word.")
+      }
+
+    } catch (error) {
+      SetDefinitions("Service Error.")
+      console.log(error)
+    }
+  }
+
+
+  const handleSubmit = (text: string) => {
+    const trimmed = text.trim();
+    if (trimmed !== "") {
+      setQuery(trimmed);
+      SetDefinitions("");
+      define(trimmed);
+    }
+  };
+
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -10,10 +49,15 @@ export default function App() {
 
         <Text style={styles.paragraph}>why dont we find out now.</Text>
       </View>
-      <ScrollView style={styles.scrollView}>
 
+      <View style={{alignSelf: 'stretch',}}>
+        <Text style={styles.subTitle}>{query}</Text>
+      </View>
+
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.paragraph}>{definitions}</Text>
       </ScrollView>
-      <InputField />
+      <InputField onSubmit={handleSubmit} />
       <StatusBar style="auto" />
     </View>
   );
@@ -28,23 +72,36 @@ const styles = StyleSheet.create({
   },
 
   scrollView: {
-    paddingTop: 150,
-    padding: 18,
+    alignSelf: 'stretch',
+    marginRight: 20,
+    marginLeft: 20,
+    borderTopWidth:1,
+
   },
 
   header: {
-    margin: 50,
+    margin: 15,
+    marginTop:50,
   },
 
   title: {
     color: '#1A2A80',
     fontSize: 35,
     marginBottom: 12,
-    fontWeight: "500"
+    fontWeight: "800"
+  },
+
+  subTitle:{
+    color: '#1A2A80',
+    fontSize: 25,
+    fontWeight: "800",
+    padding: 12,
+    marginLeft:10,
+
   },
 
   paragraph: {
     fontSize: 15,
-    color: '#B1AB86'
+    color: '#B1AB86',
   }
 });
--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,12 +1,20 @@
+import React, { useState } from 'react';
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet,TextInput, Text, TouchableHighlight, KeyboardAvoidingView, View } from 'react-native';
 
-export default function InputField() {
+type Props = {
+    onSubmit?: (text: string) => void;
+}
+
+export default function InputField({ onSubmit }: Props) {
+  const [input, setInput] = useState('');
+
   return (
     <KeyboardAvoidingView behavior='padding'>
       <View style={styles.searchBar}>
-        <TextInput style={styles.inputField} placeholder="Look for the definition here:" />
-        <TouchableHighlight style={styles.submitBtn}>
+        <TextInput value={input} onChangeText={setInput}  style={styles.inputField} placeholder="Look for the definition here:" />
+        <TouchableHighlight onPress={() => onSubmit?.(input)}  style={styles.submitBtn} >
           <Text style={{color: '#fff'}}>Search</Text>
         </TouchableHighlight>
         <StatusBar style="auto" />
@@ -21,14 +29,18 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 20,
+    marginBottom: 25,
+    paddingTop: 10,
+    borderTopWidth:1,
+    marginTop:15
   },
 
   inputField: {
     marginRight: 20,
     padding: 10,
     borderWidth: 1,
-    borderRadius: 25
+    borderRadius: 25,
+    width: 300,
   },
 
   submitBtn: {
